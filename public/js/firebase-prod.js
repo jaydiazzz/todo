b2d239ca542a51aa143ad24295925a58f79fbc15
jaydiazzz <jaydiazzz10@gmail.com>
2018-08-17 09:30:17 -0700

    Update firebase-prod.js

--- public/js/firebase-prod.js
+++ public/js/firebase-prod.js
@@ -1,7 +1,11 @@
-
-console.log( 'Set up firebase config for production' );
-
-const firebaseConfig = {} // eslint-disable-line
+const firebaseConfig = {
+	apiKey            : 'AIzaSyDP8BHncbHHTfI4cnPYq9J_ildihXMjMkc',
+	authDomain        : 'todo-app-48d87.firebaseapp.com',
+	databaseURL       : 'https://todo-app-48d87.firebaseio.com',
+	projectId         : 'todo-app-48d87',
+	storageBucket     : '',
+	messagingSenderId : '351452716587'
+};
 
 // Uncomment when you setup firebaseConfig
-// firebase.initializeApp( firebaseConfig );
+firebase.initializeApp( firebaseConfig );
